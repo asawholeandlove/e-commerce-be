c34854fac96b4da7c4772cccbafc26f55071284b
dangthaii <thaidv@hivetech.vn>
2024-06-14 15:13:40 +0700

    env config

--- src/helpers/check.connect.js
+++ src/helpers/check.connect.js
@@ -17,9 +17,6 @@ const checkOverload = () => {
     const numCores = os.cpus().length;
     const memoryUsage = process.memoryUsage().rss;
 
-    console.log(`Number of connections:: ${numConnection}`);
-    console.log(`Memory usage:: ${memoryUsage / 1024 / 1024} MB`);
-
     const maxConnections = numCores * 5;
     if (numConnection > maxConnections) {
       console.log(`Overload ${numConnection} connections`);

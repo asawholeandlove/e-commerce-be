781f16675d7ac406237c3b4937730e67242c4bcc
dangthaii <thaidv@hivetech.vn>
2024-06-14 10:07:06 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+
+const app = express();
+
+// init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Welcome to the API",
+    metadata: "Hello nè".repeat(10),
+  });
+});
+
+// handle error
+
+module.exports = app;
